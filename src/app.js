d97d3679559e86afdef1aa87af5361ec95ef6dce
SwagataChatterjee68 <ranichatterjee21112@gmail.com>
2026-01-10 17:58:49 +0530

    added some changes

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5175",
   credentials: true
 }));
 app.use(express.json());
